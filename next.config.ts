5c0c5633e502e187d56db35863f38f9faf2f3e65
apple <bitecodes.global@gmail.com>
2025-07-08 16:53:26 +0530

    Upddated url 1

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
+
+// next.config.js
+console.log('Using API URL:', process.env.NEXT_PUBLIC_API_URL);
+
+module.exports = {
+  reactStrictMode: true,
+};
+
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
